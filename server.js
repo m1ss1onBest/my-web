4b4650e1399eeb7344854232e3d06024edd84a9c
m1ss1onBest <m1poisthbexxxx@gmail.com>
2025-02-26 12:36:21 +0200

    feature/nav-and-header

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const PORT = 8300;
 
-app.use(express.static("public/styles"))
+app.use(express.static("public"))
 app.get("*", (req, res) => {
     let filepath = "src/views" + (req.url === "/" ? "/index.html" : req.url);
     fs.readFile(filepath, 'utf-8', (err, content) => {
